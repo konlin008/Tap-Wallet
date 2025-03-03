17700db3f4a3b32701daed9d4b5f88e0c758fa51
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2025-03-03 12:21:11 +0530

    reload 404 solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
